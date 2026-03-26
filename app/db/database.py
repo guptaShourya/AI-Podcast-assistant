3ccb1c9bd80cb341631e9862dca73b80b3139072
Shourya Gupta <apid.shourya@gmail.com>
2026-03-26 16:19:35 +0530

    Add SSL support for Azure database connections

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,17 @@
+import ssl
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.config import settings
 
-engine = create_async_engine(settings.database_url, echo=False)
+connect_args = {}
+if "azure.com" in settings.database_url:
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    connect_args["ssl"] = ssl_context
+
+engine = create_async_engine(settings.database_url, echo=False, connect_args=connect_args)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
